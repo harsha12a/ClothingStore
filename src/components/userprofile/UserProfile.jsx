a39d10b73ead118c43d3442d811c80a5eadd8b25
harha12a <harsha.pss2005@gmail.com>
2024-11-04 17:47:37 +0530

    chore:profile

--- src/components/userprofile/UserProfile.jsx
+++ src/components/userprofile/UserProfile.jsx
@@ -7,9 +7,15 @@ function UserProfile() {
   return (
     <div className='pt-5'>
         <p className="display-1 text-center">{status? "Welcome "+user.username : "Please Login"}</p>
-        <div className='col-4 m-auto card'>
-          <p className=" text-center">{status? "Email - "+user.email : ""}</p>
-          <p className=" text-center">{status? "Name - "+user.name : ""}</p>
+        <div className='col-4 m-auto d-flex flex-row align-items-center flex-wrap justify-content-evenly'>
+          <img src="https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_640.png" alt="ProfImg" width={'150px'} className='rounded-pill'/>
+          {status && user && (
+            <div className=''>
+              <p className="text-center"><span className='fw-bold'>Email</span> - {user.email}</p>
+              <p className="text-center"><span className='fw-bold'>Name</span> - {user.name}</p>
+              <p className='text-center'><span className='fw-bold'>Contact no.</span> - {user.contact}</p>
+            </div>
+          )}
         </div>
         <Cart />
     </div>
