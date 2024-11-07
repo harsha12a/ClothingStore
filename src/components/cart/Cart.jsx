9a7a31340fdce0d1577a4a71632cde5d90d83c02
harha12a <harsha.pss2005@gmail.com>
2024-11-07 20:51:33 +0530

    chore:final cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
 function Cart() {
+  let user = sessionStorage.getItem("user");
+  user = JSON.parse(user);
+  let cart = user.cart;
+  let navigate = useNavigate()
   return (
-    <div className='text-center'>Cart</div>
-  )
+    <div>
+      <p className="text-center mt-5 display-3">Cart</p>
+      <div className="d-flex flex-wrap justify-content-evenly">
+        {cart.map((products, ind) => {
+          return (
+            <div
+              className="p-3 d-flex flex-column justify-content-center m-3 align-items-center border border-dark"
+              key={ind}
+            >
+              <p className="text-center">{products.name}</p>
+              <img
+                src={products.image}
+                alt={products.name}
+                style={{ maxWidth: "100%", width: "300px" }}
+              />
+              <h5 className="text-center">Price - ${products.price}</h5>
+              <button className="btnhov">Remove from cart</button>
+            </div>
+          );
+        })}
+      </div>
+      <div className="d-flex justify-content-center m-5">
+        <button className="viewmore text-center px-5 py-3" onClick={()=>{navigate('../../')}}>Checkout more products</button>
+      </div>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
--- src/components/userprofile/UserProfile.jsx
+++ src/components/userprofile/UserProfile.jsx
@@ -1,25 +1,60 @@
-import React, { useContext } from 'react'
-import { loginContext } from '../../context/loginContext'
-import Cart from '../cart/Cart'
+import React, { useContext, useEffect, useState } from "react";
+import { loginContext } from "../../context/loginContext";
+import { useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./UserProfile.css";
 function UserProfile() {
-    let user = JSON.parse(sessionStorage.getItem('user'))
-    let {status} = useContext(loginContext)
-  return (
-    <div className='mt-5'>
-        <p className="display-1 text-center">{status? "Welcome "+user.username : "Please Login"}</p>
-        <div className='m-auto d-flex flex-row align-items-center flex-wrap justify-content-evenly bg-warning p-5 col-md-6 col-sm-12'>
-          <img src="https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_640.png" alt="ProfImg" style={{maxWidth:'100%',width:'200px'}} className='rounded-pill'/>
-          {status && (
-            <div className=''>
-              <p className="text-center"><span className='fw-bold'>Email</span> - {user.email}</p>
-              <p className="text-center"><span className='fw-bold'>Name</span> - {user.name}</p>
-              <p className='text-center'><span className='fw-bold'>Contact no.</span> - {user.contact}</p>
-            </div>
-          )}
-        </div>
-        <Cart />
+  let user = JSON.parse(sessionStorage.getItem("user"));
+  let navigate = useNavigate();
+  let location = useLocation();
+  let [cart, setCart] = useState(false);
+  useEffect(() => {
+    if (location.pathname === "/profile/cart") {
+      setCart(true);
+    } else if (location.pathname === "/profile") {
+      setCart(false);
+    }
+  }, [location.pathname]);
+  let { status } = useContext(loginContext);
+  return !cart ? (
+    <div className="mt-5">
+      <p className="display-1 text-center">
+        {status ? "Welcome " + user.username : "Please Login"}
+      </p>
+      <div className="m-auto d-flex flex-row align-items-center flex-wrap justify-content-evenly bg-warning p-5 col-md-6 col-sm-12">
+        <img
+          src="https://cdn.pixabay.com/photo/2018/11/13/21/43/avatar-3814049_640.png"
+          alt="ProfImg"
+          style={{ maxWidth: "100%", width: "200px" }}
+          className="rounded-pill"
+        />
+        {status && (
+          <div className="text-center">
+            <p>
+              <span className="fw-bold">Email</span> - {user.email}
+            </p>
+            <p>
+              <span className="fw-bold">Name</span> - {user.name}
+            </p>
+            <p>
+              <span className="fw-bold">Contact no.</span> - {user.contact}
+            </p>
+          </div>
+        )}
+      </div>
+      <p
+        className="text-center p-3 display-6 col-lg-4 col-sm-10 col-md-6 m-auto my-5 cart"
+        onClick={() => {
+          navigate("./cart");
+        }}
+      >
+        My cart
+      </p>
     </div>
-  )
+  ) : (
+    <Outlet />
+  );
 }
 
-export default UserProfile
+export default UserProfile;
